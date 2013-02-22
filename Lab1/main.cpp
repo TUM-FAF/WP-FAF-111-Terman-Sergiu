69457fd69e4883592181812ae06555ac24a0fcf3
sergiu.terman <terman.sergiu@gmail.com>
2013-02-22 19:39:05 +0200

    Added A resize action window while horisontal resizing

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -120,15 +120,7 @@ LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam) {
             * Draw Food List (In a input box)
             */
 
-            /*HGDIOBJ hfDefault1 = GetStockObject(DEFAULT_GUI_FONT);
-            HFONT hFont = CreateFont(25,0,0,0,FW_DONTCARE,FALSE,TRUE,FALSE,DEFAULT_CHARSET,OUT_OUTLINE_PRECIS,
-                CLIP_DEFAULT_PRECIS,NULL, VARIABLE_PITCH,TEXT("Impact"));
 
-            SendMessage(hFoodList,
-                WM_SETFONT,
-                (WPARAM)hfDefault1,
-                MAKELPARAM(FALSE,0));
-            */
             hFoodList = CreateWindowEx(WS_EX_CLIENTEDGE,
                 "EDIT",
                 "",
@@ -143,9 +135,6 @@ LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam) {
                 GetModuleHandle(NULL),
                 NULL);
 
-            /*SendMessage(hFoodList,
-                WM_SETTEXT,INPUT_TEXT_SHOW_FOOD,
-                (LPARAM) food);*/
 
             /**
             * Draw main Input food field
@@ -153,16 +142,16 @@ LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam) {
 
             hInputFood = CreateWindowEx(
                 (DWORD)NULL,
-                TEXT("edit"),                                                   // The class name required is edit
-                "",                                                             // Default text.
-                WS_VISIBLE | WS_CHILD | WS_BORDER,                              // Textbox styles
+                TEXT("edit"),
+                "",
+                WS_VISIBLE | WS_CHILD | WS_BORDER,
                 120,
-                150,                                                         // the left and top co-ordinates
+                150,
                 190,
-                25,                                                        // width and height
-                hWnd,                                                           // parent window handle
-                (HMENU)INPUT_TEXT_ADD_FOOD,                                          // the ID of your editbox
-                GetModuleHandle(NULL),                                                          // the instance of your application
+                25,
+                hWnd,
+                (HMENU)INPUT_TEXT_ADD_FOOD,
+                GetModuleHandle(NULL),
                 NULL);
         }
         break;
@@ -204,27 +193,13 @@ LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam) {
 
             if (GetDlgCtrlID(hwnd) == INPUT_TEXT_ADD_FOOD) {
                 color = CreateSolidBrush(RGB(225, 225, 225));
-                SetTextColor(hdc, RGB(0, 0, 255));                            // Text color
-                SetBkMode(hdc, TRANSPARENT);                                // EditBox Backround Mode
+                SetTextColor(hdc, RGB(0, 0, 255));
+                SetBkMode(hdc, TRANSPARENT);
                 SetBkColor(hdc,(LONG)color);
             }
             return (LONG) color;
         }
         break;
-
-        case WM_CTLCOLORBTN: {
-            HDC hdc = (HDC)wParam;
-            HWND hwnd = (HWND)lParam;
-            HBRUSH color;
-
-            if (GetDlgCtrlID(hwnd) == BUTTON_DISPLAY_FOOD_NR) {
-                color = CreateSolidBrush(RGB(225, 225, 225));
-                SetTextColor(hdc, RGB(0, 0, 255));                            // Text color
-                SetBkMode(hdc, TRANSPARENT);                                // EditBox Backround Mode
-                SetBkColor(hdc,(LONG)color);
-                return (LRESULT) color;
-            }
-        }
 
 		case WM_COMMAND: {
             switch(LOWORD(wParam)) {
@@ -282,6 +257,20 @@ LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam) {
         }
         break;
 
+        case WM_SIZE: {
+            INT nWidth = LOWORD(lParam);
+            HWND hFunnyButton = GetDlgItem(hWnd, BUTTON_DISPLAY_FOOD_NR);
+
+            MoveWindow(hFunnyButton, 10, 180, nWidth - 17, 40, TRUE);
+
+            HWND hShowFoodInput = GetDlgItem(hWnd, INPUT_TEXT_SHOW_FOOD);
+            HWND hAddFood = GetDlgItem(hWnd, INPUT_TEXT_ADD_FOOD);
+
+            MoveWindow(hShowFoodInput, 10, 40, nWidth - 18, 100, TRUE);
+            MoveWindow(hAddFood, 120, 150, nWidth - 128, 25, TRUE);
+        }
+        break;
+
 		case WM_DESTROY: {
 			PostQuitMessage(0);
 			return 0;
@@ -293,74 +282,3 @@ LRESULT CALLBACK WinProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam) {
 }
 
 
-
-//RECT rect;
-                /**
-                * Drawing Buttons
-                */
-
-
-
-                /*
-                BeginPaint(hWnd, &Ps);
-                SetRect(&rect, 50, 50,700,200);
-                DrawText(hdc, TEXT("Drawing Text with Impact"), -1,&rect, DT_NOCLIP);
-
-                HFONT hFont = CreateFont(25,0,0,0,FW_DONTCARE,FALSE,TRUE,FALSE,DEFAULT_CHARSET,OUT_OUTLINE_PRECIS,
-                CLIP_DEFAULT_PRECIS,NULL, VARIABLE_PITCH,TEXT("Impact"));
-                SetRect(&rect, 50, 100, 700, 200);
-
-                SelectObject(hdc, hFont);
-                SetBkMode(hdc, OPAQUE);
-                SetBkColor(hdc, RGB(0,255,255));
-                SetTextColor(hdc, RGB(255,0,0));
-                DrawText(hdc, TEXT("Drawing Text with Impact"), -1,&rect, DT_NOCLIP);
-                */
-
-                /*hEdit=CreateWindowEx(WS_EX_CLIENTEDGE,
-				"EDIT",
-				"",
-				WS_CHILD|WS_VISIBLE|
-				ES_MULTILINE|ES_AUTOVSCROLL|ES_AUTOHSCROLL,
-				50,
-				100,
-				200,
-				100,
-				hWnd,
-				(HMENU)IDC_MAIN_EDIT,
-				GetModuleHandle(NULL),
-				NULL);
-
-				HGDIOBJ hfDefault=GetStockObject(DEFAULT_GUI_FONT);
-                SendMessage(hEdit,
-                    WM_SETFONT,
-                    (WPARAM)hfDefault,
-                    MAKELPARAM(FALSE,0));
-
-                SendMessage(hEdit,
-                    WM_SETTEXT,IDC_MAIN_EDIT,
-                    (LPARAM)"Insert text here...");
-
-                HWND hWndButton=CreateWindowEx(NULL,
-				"BUTTON",
-				"OK",
-				WS_TABSTOP|WS_VISIBLE|
-				WS_CHILD|BS_DEFPUSHBUTTON|BS_TOP,
-				10,
-				10,
-				100,
-				25,
-				hWnd,
-				(HMENU)IDC_MAIN_BUTTON,
-				GetModuleHandle(NULL),
-				NULL);
-
-			SendMessage(hWndButton,
-				WM_SETFONT,
-				(WPARAM)hfDefault,
-				MAKELPARAM(FALSE,0));*/
- /*MessageBox(NULL,
-                    buffer,
-                    "Information",
-                    MB_ICONINFORMATION);
-                    */
